d3ab141e55099aba06fb38dffb3d7255906dabe1
Ganglion17 <155130776+Ganglion17@users.noreply.github.com>
2024-01-03 10:48:48 +0500

    Attributes handling fix (#12)

---
+++ spec/prism_checker/node/base_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'prism_checker/checker'
+
+describe PrismChecker::Node::Base do
+  describe '#path' do
+    it 'returns node path' do
+      checker = PrismChecker::Checker.new
+      checker.send(:prepare, nil, { a: { b: 'foo' } })
+      node = checker.root.children[:a].children[:b]
+
+      expect(node.path).to eq %i[root a b]
+    end
+  end
+end
