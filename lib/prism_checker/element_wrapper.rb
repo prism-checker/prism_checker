1d97fe63373ffdbeb5b22ba77ade28153b928bf8
Ganglion-17 <ganglion1717@gmail.com>
2024-01-03 10:30:30 +0500

    Attributes handling fix

--- lib/prism_checker/element_wrapper.rb
+++ lib/prism_checker/element_wrapper.rb
@@ -12,17 +12,44 @@ module PrismChecker
       @element[:class]
     end
 
+    def checked
+      @element.checked?
+    end
+
+    def disabled
+      @element.disabled?
+    end
+
+    def multiple
+      @element.multiple?
+    end
+
+    def readonly
+      @element.readonly?
+    end
+
+    def selected
+      @element.selected?
+    end
+
     def method_missing(method, *args)
       if @element.respond_to?(method)
         return @element.send(method, *args)
       end
 
-      if (ItemClassifier.element_checkbox?(@element) || ItemClassifier.element_radio?(@element)) && method == :checked
-        return @element.checked?
+      # if (ItemClassifier.element_checkbox?(@element) || ItemClassifier.element_radio?(@element)) && method == :checked
+      #   return @element.checked?
+      # end
+
+      ret = @element[method]
+
+      if ret.nil? && !method.start_with?('data-')
+        # return '' if method.start_with?('data-')
+
+        raise NoMethodError, "unknown attribute '#{method}'"
       end
 
-
-      @element[method]
+      ret
     end
 
     def respond_to_missing?(*_args)
